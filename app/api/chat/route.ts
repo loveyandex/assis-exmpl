1d0501ea180e6a10c9e136a2ce6ffff459c5fc75
Not yet Started <53381292+loveyandex@users.noreply.github.com>
2025-08-21 06:54:58 +0000

    llama4

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,26 @@
-import { openai } from "@ai-sdk/openai";
 import {
   streamText,
   UIMessage,
   convertToModelMessages,
 } from "ai";
 
+import { createOpenAI } from '@ai-sdk/openai'
+
+
+const openai = createOpenAI({
+      baseURL: "https://api.cerebras.ai/v1",
+      apiKey: process.env.CEREBRAS_API_KEY
+})
+
+
+
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: openai.chat("llama-4-scout-17b-16e-instruct"), // Your local model name
     messages: convertToModelMessages(messages),
   });
 
   return result.toUIMessageStreamResponse();
-}
+} 
+ 
