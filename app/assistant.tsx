2cb1ee9fe9a5418b9741e3163f3e46b08065cd58
Your Name <you@example.com>
2025-08-29 23:45:30 +0330

    sm:rounded-lg removed

--- app/assistant.tsx
+++ app/assistant.tsx
@@ -61,13 +61,13 @@ export const Assistant = ({
               <Breadcrumb>
                 <BreadcrumbList>
                   <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="https://www.assistant-ui.com/docs/getting-started" target="_blank" rel="noopener noreferrer">
-                      Build Your Own ChatGPT UX
+                    <BreadcrumbLink href="#">
+                      sdoniisndisd sdnosi dsd 
                     </BreadcrumbLink>
                   </BreadcrumbItem>
                   <BreadcrumbSeparator className="hidden md:block" />
                   <BreadcrumbItem>
-                    <BreadcrumbPage>Starter Template</BreadcrumbPage>
+                    <BreadcrumbPage>Q </BreadcrumbPage>
                   </BreadcrumbItem>
                 </BreadcrumbList>
               </Breadcrumb>
